0bd19eeec27b1fca7c3c21fa5e59b88b294ae402
devMoriarty <bl4ckvill@gmail.com>
2021-05-24 15:38:41 +0900

    임시

--- FruitsPang/Classes/MenuScene.cpp
+++ FruitsPang/Classes/MenuScene.cpp
@@ -27,8 +27,14 @@ bool MenuScene::init()
 	background->setScaleY(rY);
 
 	auto Normal = Label::createWithTTF("Normal", "fonts/Jellee-Roman.ttf", 24);
+	auto normal_item = MenuItemLabel::create(Normal);
+	
 	auto Reverse = Label::createWithTTF("Reverse", "fonts/Jellee-Roman.ttf", 24);
+	auto reverse_item = MenuItemLabel::create(Reverse);
+
 	auto Blink = Label::createWithTTF("Blink", "fonts/Jellee-Roman.ttf", 24);
+	auto blink_item = MenuItemLabel::create(Blink);
+
 	Normal->setTextColor(Color4B::WHITE);
 	Reverse->setTextColor(Color4B::WHITE);
 	Blink->setTextColor(Color4B::WHITE);
@@ -44,12 +50,8 @@ bool MenuScene::init()
 	menu->setPosition(Vec2::ZERO);
 	addChild(menu, 1);*/
 
-	auto Normal = MenuItemLabel::create(Normal, CC_CALLBACK_1(MenuScene::menuCallback, this));
-	auto Reverse = MenuItemLabel::create(Reverse, CC_CALLBACK_1(MenuScene::menuCallback, this));
-	auto Blink = MenuItemLabel::create(Blink, CC_CALLBACK_1(MenuScene::menuCallback, this));
-
-	auto menu = Menu::create(Normal, Reverse, Blink, nullptr);
-	menu->alignItemsvertically();
+	auto menu = Menu::create(normal_item, reverse_item, blink_item, nullptr);
+	menu->alignItemsVertically();
 	this->addChild(menu);
 	menu->setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
 
